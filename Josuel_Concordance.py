4bdaff8ce1cedc626a17b9e870a1a2a35e5857eb
Josuel <joshlixmus@gmail.com>
2015-03-16 02:36:21 -0400

    Update Josuel_Concordance.py

--- Josuel_Concordance.py
+++ Josuel_Concordance.py
@@ -2,19 +2,23 @@
 
 # library that breaks text into parts
 # note that the module nltk needs to be installed first in the directory
-import nltk 
+
+import nltk
 import string
 
-with open('c:/Python27/fileIn.txt', 'r') as in_file:
+from_file=str(raw_input("Enter the name of the file to read: "))
+# c:/Python27/fileIn.txt
+
+with open(from_file, 'r') as in_file:
     text = in_file.read()
     f = nltk.sent_tokenize(text)
-
 
 # This code deals with the proble of parenthesis
 for item in range(len(f)-1):
 	if '(' in f[item] and ')' in f[item+1]:
 		f[item] += ' ' + f[item+1]
 		f.remove(f[item+1])
+
 
 # count and display results of counted words
 myDict = {}
@@ -51,10 +55,27 @@ for line in f:
             myDict[word] = []
         myDict[word].append(linenum)
 
-print "%-15s %5s  %s" %("Word", 'Count', "Line Numbers")
 
-print "%-15s %5s  %s" %("====", '=====', "============")
+to_file = str(raw_input("Enter the name of the file to write: "))
+# c:/Python27/fileOut.txt
 
-for key in sorted(myDict):
-    print '%-15s %5d: %s' % (key, len(myDict[key]), myDict[key])
+text1 = "%-15s %5s  %s" %("Word", 'Count', "Line Numbers")
+text2 = "%-15s %5s  %s" %("====", '=====', "============")
+
+
+with open(to_file, 'w') as Out_file:
+    text1 = Out_file.write(text1)
+    Out_file.write("\n")
+    text2 = Out_file.write(text2)
+    Out_file.write("\n")
+    #text3 = Out_file.write(text3)
+
+
+    for key in sorted(myDict):
+        text3 = '%-15s %5d: %s' % (key, len(myDict[key]), myDict[key])
+        Out_file.write(text3)
+        Out_file.write("\n")
+
+    Out_file.close()
+
 
